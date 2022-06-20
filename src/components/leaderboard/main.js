47e083de2b332c3e10438cf30a634b63ee93f83e
Obieda Akilan <obieda.jehad@gmail.com>
2022-06-20 13:51:41 +0300

    post and personal

--- src/components/leaderboard/main.js
+++ src/components/leaderboard/main.js
@@ -1,6 +1,18 @@
-import React, { useContext } from 'react'
-export default function leaderboard() {
-    const leaderboard = useContext(leaderboardContext);
+import React, { useState, useEffect } from 'react'
+export default function leaderboard(props) {
+    const API = "http://isupport-server.herokuapp.com";
+    const [leaderboard, setLeaderboard] = useState([]);
+
+
+    // /community/:id/leaderboard  Leaderboard Information for a Community with :id=communityId
+    async function getLeaderboard(communityId) {
+        const response = await axios.get(API + `/community/${communityId}/leaderboard`);
+        setLeaderboard(response.data);
+    }
+
+    useEffect(() => {
+        getLeaderboard(props.communityId);
+    }, [])
     return (
         <div>leaderboard</div>
     )
--- src/components/personal-progress/main.js
+++ src/components/personal-progress/main.js
@@ -1,7 +1,18 @@
 import React from 'react'
 import { personalProgressContext } from '../../context/personal-progress/personalPorgressContext'
 export default function personalProgress() {
-    const personalProgress = useContext(personalProgressContext);
+    const API = "http://isupport-server.herokuapp.com";
+    const [personalProgress, setPersonalProgress] = useState([]);
+
+    // /community/:id/personalProgress
+    async function getPersonalProgress(communityId) {
+        const response = await axios.get(API + `/community/${communityId}/personalProgress`);
+        setPersonalProgress(response.data);
+    }
+
+    useEffect(() => {
+        getPersonalProgress(props.communityId);
+    }, [])
     return (
         <div>personalProgress</div>
     )
