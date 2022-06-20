07200f21be167db16ebcb20f4a9e183a2b5838aa
moathabuhamad <moathabuhamad@outlook.com>
2022-06-20 10:31:11 +0300

    pallet and font

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Posts from "./components/posts/Posts";
 
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">
+    <Posts/>
+  </div>;
 }
 
 export default App;
